17f20341974b83a3ea2b73ba377009b1c182bb20
Tokiyn <tokiyn@yahoo.com>
2020-07-19 12:43:30 -0500

    MVP

--- todos/src/main/java/com/lambdaschool/todos/services/TodosService.java
+++ todos/src/main/java/com/lambdaschool/todos/services/TodosService.java
@@ -1,6 +1,14 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.models.Todos;
+
 public interface TodosService
 {
     void markComplete(long todoid);
+
+    Todos updateTodo(Todos todo, long id);
+
+    Todos getTodoById(long id);
+
+    Todos save(Todos todo, long userid);
 }
